91b9088ec252fc4d1975fd7558b309c5de54dee6
nabeel-anwar <nabeelanwar1965@gmail.com>
2022-08-03 22:23:54 +0500

    Nested Routes and Named Routes

--- Routing/src/main.js
+++ Routing/src/main.js
@@ -3,23 +3,47 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
-import TeamMember from './components/teams/TeamMembers.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 import NotFound from './components/NotFound.vue';
 import App from './App.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    // { component: TeamsList }, // default route
-
     { path: '/', redirect: '/teams' },
-    // { path: '/teams', component: TeamsList, alias: '/' }, with alias
-    { path: '/teams', component: TeamsList },
+    {
+      name: 'teams',
+      path: '/teams',
+      component: TeamsList,
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        }, // /teams/t1
+      ],
+    }, // our-domain.com/teams => TeamsList
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMember, props: true },
-    // { path: '/:notFound(.*)', redirect: '/teams' },
+
     { path: '/:notFound(.*)', component: NotFound },
   ],
+  // routes: [
+  //   // { component: TeamsList }, // default route
+
+  //   { path: '/', redirect: '/teams' },
+  //   // { path: '/teams', component: TeamsList, alias: '/' }, with alias
+  //   // { path: '/teams', component: TeamsList },
+  //   { path: '/users', component: UsersList },
+  //   // { path: '/teams/:teamId', component: TeamMember, props: true },
+  //   {
+  //     path: '/teams',
+  //     component: TeamsList,
+  //     children: [{ path: ':teamId', component: TeamMember, props: true }],
+  //   },
+  //   // { path: '/:notFound(.*)', redirect: '/teams' },
+  //   { path: '/:notFound(.*)', component: NotFound },
+  // ],
   linkActiveClass: 'active',
 });
 
