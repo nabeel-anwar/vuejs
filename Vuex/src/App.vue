ccada89ea96128e9ea5f8ceb12d948b181bc19e1
nabeel-anwar <nabeelanwar1965@gmail.com>
2022-08-05 20:01:29 +0500

    Vuex core & Mapper Helper Function

---
+++ Vuex/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <favorite-value></favorite-value>
+    <button @click="increase({ value: 10 })">Add 10</button>
+    <change-counter></change-counter>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
+
+import { mapActions } from 'vuex';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter,
+    FavoriteValue,
+  },
+  methods: {
+    // addOne() {
+    //   // this.$store.commit('increase', { value: 10 });
+    //   this.$store.dispatch({
+    //     type: 'increase',
+    //     value: 10,
+    //   });
+    ...mapActions(['increase']),
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ Vuex/src/components/ChangeCounter.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="increase({ value: 3 })">Increase 3</button>
+  <button @click="increment">Async Inc 2</button>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  methods: {
+    // addOne() {
+    //   // this.$store.commit('increment');
+    //   // this.$store.dispatch('increment'); //Run methods in actions
+    // },
+    ...mapActions(['increment', 'increase']),
+  },
+};
+</script>
